f1e5fd9d5733ebf8fd1907b19dbfc75b5fc10f70
NischalBatas <58929522+NischalBatas@users.noreply.github.com>
2020-02-18 10:40:29 +0545

    update

--- Restapi/urls.py
+++ Restapi/urls.py
@@ -5,8 +5,8 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('products/',views.product_data),
-    path('products/<int:pk>/',views.get_product),
+    path('products/<int:id>/',views.get_product),
     path('products/add/',views.add_product),
     path('products/paginated/<int:page_num>/<int:num_data>',views.product_objects_pagination),
-    path('products/change/<int:pk>/',views.update_api_data),
+    path('products/change/<int:id>/',views.update_api_data),
     ]
